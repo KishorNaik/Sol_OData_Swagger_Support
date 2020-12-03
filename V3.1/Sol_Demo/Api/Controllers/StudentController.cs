b9b83c61dc946ef9677cfed80ab9884192685349
Kishor Naik <kishor.naik011.net@outlook.com>
2020-12-03 20:43:28 +0530

    Code Commit

---
+++ V3.1/Sol_Demo/Api/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        //[HttpGet("list")]
+        [HttpPost("list")]
+        [EnableQuery]
+        public IActionResult StudentList()
+        {
+            var studentList = new List<StudentModel>();
+            studentList.Add(new StudentModel()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Sharmila Naik",
+                Score = 98
+            });
+            studentList.Add(new StudentModel()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Mahesh Naik",
+                Score = 90
+            });
+
+            studentList.Add(new StudentModel()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Kishor Naik",
+                Score = 70
+            });
+
+            return base.Ok(studentList);
+        }
+    }
+}
---
+++ V3.1/Sol_Demo/OData.Swagger/Services/OdataSwaggerSupportServiceExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OData.Swagger.Services
+{
+    public static class OdataSwaggerSupportServiceExtension
+    {
+        public static void AddOdataSwaggerSupport(this IServiceCollection services)
+        {
+            services.AddMvcCore(options =>
+            {
+                foreach (var outputFormatter in options.OutputFormatters.OfType<OutputFormatter>().Where(x => x.SupportedMediaTypes.Count == 0))
+                {
+                    outputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+
+                foreach (var inputFormatter in options.InputFormatters.OfType<InputFormatter>().Where(x => x.SupportedMediaTypes.Count == 0))
+                {
+                    inputFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/prs.odatatestxx-odata"));
+                }
+            });
+        }
+    }
+}
